c2d37027ceeaaf1e20b5ddc31e011f882795f2ca
GLITCH LINUX <154828412+GlitchLinux@users.noreply.github.com>
2025-06-02 13:19:00 +0200

    Update QEMU_Boot_BIOS.sh

--- QEMU_Boot_BIOS.sh
+++ QEMU_Boot_BIOS.sh
@@ -3,28 +3,6 @@
 # Clear the terminal screen
 clear
 
-# Set the terminal window title to the script name
-echo -ne "\033]0;$(basename "$0")\007"
-
-# Set terminal window size (if supported by the terminal)
-#resize -s 10 180
-
-# Function to check permissions
-check_permissions() {
-  if [ "$EUID" -ne 0 ]; then
-    echo "Sorry - you need to run as root (use sudo)."
-    echo
-    echo "If you ran as root, please check your PATH environment variable includes /usr/sbin and /sbin."
-    echo
-    echo "PATH VARIABLE = $PATH"
-    echo
-    read -p "Press any key to continue..."
-    return 1
-  fi
-  echo "Administrative permissions confirmed."
-  return 0
-}
-
 # Change directory to the script location and then to ./QEMU
 #cd "$(dirname "$0")/QEMU" || exit
 
@@ -42,8 +20,9 @@ fi
 DN=$(df --output=source "$(dirname "$(realpath "$0")")" | tail -n 1 | sed 's/[0-9]*$//')
 
 # Show drive number and memory size
-echo "DRIVE NUMBER=$DN ($DEV_PATH)"
-echo "MEMORY SIZE=$MEM"
+echo "QEMU VM STARTING FROM = $DN ($DEV_PATH)"
+echo "FIRMWARE TYPE = BIOS"
+echo "AMOUNT OF RAM = $MEM"
 
 # Flush file system buffers
 sync
@@ -54,14 +33,8 @@ QEMU=qemu-system-x86_64  # Assuming using QEMU for 64-bit, adjust if needed
 # Set the QEMU command
 CMD="sudo $QEMU -L . -name 'BIOS Boot' -vga std -boot c -m $MEM -drive file=$DN,if=ide,index=0,media=disk,format=raw"
 
-# Print the command to be executed
-echo "Sending command $CMD to shell..."
-echo
-echo "THIS CONSOLE WINDOW WILL REMAIN OPEN UNTIL YOU QUIT QEMU"
-echo
-
 # Execute the QEMU command
-echo $CMD
+# echo $CMD
 eval "$CMD" 2>/dev/null
 
 # Check if QEMU failed to run
@@ -74,3 +47,4 @@ fi
 # lsblk or udevadm trigger --action=add may be used if needed.
 
 exit 0
+
